1406476c92718b2ff85dc6dedac3c9b4d776298b
UncleSamNunpuia <70100222+UncleSamNunpuia@users.noreply.github.com>
2026-06-28 02:36:15 +0530

    update 2_JS/B_authenticatedPages.js

--- 2_JS/B_authenticatedPages.js
+++ 2_JS/B_authenticatedPages.js
@@ -31,3 +31,62 @@
     // window.location.href = "login.html";
   }
 })();
+
+const GAS_URL =
+  "https://script.google.com/macros/s/AKfycbybot_jsane8OaXdYBSyoROy14s2NrTw6rj_Cmv3JszHjKbe7kp7vxVeilMe5xc17eLig/exec";
+
+document
+  .getElementById("logoutBtn")
+  .addEventListener("click", logout);
+
+async function logout() {
+
+  if (!confirm("Are you sure you want to logout?")) {
+    return;
+  }
+
+  const token = sessionStorage.getItem("auth_token");
+
+  try {
+
+    const body = new URLSearchParams({
+
+      action: "logout",
+
+      token: token
+
+    });
+
+    const response = await fetch(GAS_URL, {
+
+      method: "POST",
+
+      body: body
+
+    });
+
+    const result = await response.json();
+
+    console.log(result);
+
+  }
+  catch (err) {
+
+    console.error("Logout Error:", err);
+
+  }
+  finally {
+
+    // Remove only these keys
+
+    sessionStorage.removeItem("auth_token");
+
+    sessionStorage.removeItem("username");
+
+    sessionStorage.removeItem("role");
+
+    window.location.replace("../index.html");
+
+  }
+
+}
